4ea63a4894cee7e8938f7bef5a7f2ba932a6dae5
Jason Cook <the.root.user@gmail.com>
2020-03-19 23:50:11 -0400

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,48 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+import '@testing-library/jest-dom'
+
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("contains a first name", () => {
+  const { getByText } = render(<App />);
+
+  const firstName = getByText(/first name/i)
+
+  expect(firstName).toBeInTheDocument()
+});
+
+test("contains a last name", () => {
+  const { getByText } = render(<App />);
+
+  const lastName = getByText(/last name/i)
+
+  expect(lastName).toBeInTheDocument()
+});
+
+test("contains an email field", () => {
+  const { getByText } = render(<App />);
+
+  const email = getByText(/email/i)
+
+  expect(email).toBeInTheDocument()
+});
+
+test("contains a message", () => {
+  const { getByText } = render(<App />);
+
+  const message = getByText(/message/i)
+
+  expect(message).toBeInTheDocument()
+});
+
+test("contains a submit button", () => {
+  const { getByText } = render(<App />);
+
+  const submitButton = getByText( /submit/i )
+
+  expect(submitButton).toBeInTheDocument()
 });
